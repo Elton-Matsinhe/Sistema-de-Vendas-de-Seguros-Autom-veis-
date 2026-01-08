d4981c5db96ee493d5c706be68ff73c2d113ac3b
Elton Matsinhe <eltonmatsinhe8330@gmail.com>
2026-01-08 15:26:27 +0200

    fix: remove person_two e corrige build

--- src/utils/images.js
+++ src/utils/images.js
@@ -19,7 +19,6 @@ import wealth from "../assets/icons/wealth.svg";
 
 // person images
 import person_one from "../assets/images/person_one.jpg";
-import person_two from "../assets/images/person_two.jpg";
 import person_three from "../assets/images/person_three.jpg";
 import person_four from "../assets/images/person_four.jpg";
 
@@ -28,6 +27,6 @@ export const iconsImgs = {
 }
 
 export const personsImgs = {
-    person_one, person_two, person_three, person_four
+    person_one, person_three, person_four
 };
 
